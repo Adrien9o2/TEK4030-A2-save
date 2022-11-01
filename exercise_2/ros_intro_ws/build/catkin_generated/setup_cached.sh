610e71c17e65e07fada556b09236b2bbdf51fe48
AdrienK <adrienkomaroff@hotmail.fr>
2022-11-01 19:14:15 +0100

    works but no rqt_plot

--- exercise_2/ros_intro_ws/build/catkin_generated/setup_cached.sh
+++ exercise_2/ros_intro_ws/build/catkin_generated/setup_cached.sh
@@ -7,4 +7,10 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/adrienk/TEK4030-A2/exercise_2/ros_intro_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/adrienk/TEK4030-A2/exercise_2/ros_intro_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/adrienk/TEK4030-A2/exercise_2/ros_intro_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/adrienk/TEK4030-A2/exercise_2/ros_intro_ws/build'
+export PYTHONPATH="/home/adrienk/TEK4030-A2/exercise_2/ros_intro_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES='/home/adrienk/TEK4030-A2/exercise_2/ros_intro_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/adrienk/TEK4030-A2/exercise_2/ros_intro_ws/src:$ROS_PACKAGE_PATH"
